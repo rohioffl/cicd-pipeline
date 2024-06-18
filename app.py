edfb11a3d80a513a857b855b69f8cf8293b19119
rohit <rohitpt2003@gmail.com>
2024-06-19 00:44:25 +0530

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 application.secret_key = os.getenv('SECRET_KEY')
 
 # MySQL Configuration
-db_host = os.getenv('DB_HOST')
+db_host = "mysql-svc.mysql-flask.svc.cluster,local"
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 db_name = os.getenv('DB_NAME')
